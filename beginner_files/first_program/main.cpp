b2a1c4ae9c19bc4a614760132506f5d6514faf5f
Ina Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2015-08-30 15:05:41 +0200

    Started on"Hello World"

---
+++ beginner_files/first_program/main.cpp
@@ -0,0 +1,16 @@
+//First program in C++
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+// convert the text argv[1] to double using atof:
+    double r = atof(argv[1]);
+    double s = sin(r);
+    cout << "Hello World! sin("<< r <<")=" << s << endl;
+    return 0;
+}
+
