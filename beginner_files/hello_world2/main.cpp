f9cd57a35f2e954d021d1489acdd685879f033ac
Ina Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2015-08-30 16:51:08 +0200

    Added exeption handling version

--- beginner_files/hello_world2/main.cpp
+++ beginner_files/hello_world2/main.cpp
@@ -1,4 +1,5 @@
 //Second way of writing "Hello World"
+/*
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
@@ -12,4 +13,32 @@ int main(int argc, char*argv[])
     std::cout << "Hello, World! sin(" << r << ")=" << s << std::endl;
     return 0;
 }
+*/
+
+//Hello world with exeption handling
+
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+int main (int argc, char* argv[])
+{
+    //Read in output, abort if there are too few command-line arguments
+    if( argc<=1 ){
+        cout << "Bad usage: " << argv[0] <<
+                "\n Read also a number on the same line, e.g., prog.exe 0.2" << endl;
+        exit(1); //here the program stops.
+    }
+    //convert the text argv[1] to double using atof:
+    double r = atof(argv[1]);
+    double s = sin(r);
+    cout << "Hello world! sin(" << r << ")=" << s << endl;
+    //sucsess
+    return 0;
+}
+
+
+
 
