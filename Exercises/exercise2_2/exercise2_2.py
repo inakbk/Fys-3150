8215701da3469daa608a22d4a66ff68eee798513
Ina Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2015-08-30 19:04:52 +0200

    Started exer2_2

---
+++ Exercises/exercise2_2/exercise2_2.py
@@ -0,0 +1,20 @@
+# Exercise 2.2 Fys3150
+
+
+N = 10000000
+sum_up = 0
+sum_down = 0
+
+n=1
+while n != (N+1):
+	sum_up += 1./n
+	n += 1
+
+n = N
+while n != 0:
+	sum_down += 1./n
+	n -= 1
+
+print sum_up
+print sum_down
+
---
+++ Exercises/exercise2_2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(argc, argv[])
+{
+    int n, N, sum_up, sum_down;
+    n = 1
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
