b2c2f4fc5cde8ab2ace3f5c5ab208e9ad087e848
Ina Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2015-09-03 13:54:54 +0200

    dynamic array displayed

--- beginner_files/dynamic_arrays/main.cpp
+++ beginner_files/dynamic_arrays/main.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    vector<int> DynArrNums (3);
+
+    DynArrNums[0] = 365;
+    DynArrNums[1] = -421;
+    DynArrNums[2] = 789;
+
+    cout << "number of integers in array: " << DynArrNums.size() << endl;
+
+    cout << "enter another number for the array: " << endl;
+    int AnotherNum = 0;
+    cin >> AnotherNum;
+    DynArrNums.push_back(AnotherNum);
+
+    cout << "Number of integers in array: " << DynArrNums.size() << endl;
+    cout << "Last element in array: ";
+    cout << DynArrNums[DynArrNums.size() - 1] << endl;
+    cout << DynArrNums[DynArrNums.size() - 2] << endl;
+    cout << DynArrNums[DynArrNums.size() - 3] << endl;
+    cout << DynArrNums[DynArrNums.size() - 4] << endl;
     return 0;
 }
 
