d9760902140fcb2438fbe6192eb8fa27d0ffb264
Ina Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2015-09-07 15:12:05 +0200

    testing pointers

--- beginner_files/test/main.cpp
+++ beginner_files/test/main.cpp
@@ -4,8 +4,14 @@ using namespace std;
 
 int main()
 {
-    for (int Counter = 0; Counter <= 10; ++Counter)
-        cout << Counter << " ";
+    int Number = 3;
+    int* pNum1 = &Number; //points to adress of number
+    *pNum1 = 20; //value of address that pNum1 points to is now 20-->Number=20
+
+    int* pNum2 = pNum1; // points to same location as pNum1
+    Number *= 2; // Number = 20*2 = 40
+
+    cout << *pNum2 << endl; // displays value of address which is Number = 40
     return 0;
 }
 
