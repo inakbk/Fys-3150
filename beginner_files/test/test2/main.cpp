90df3d762f2d84903e89f9f97b2465ae6e5378af
Ina Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2015-09-10 12:00:05 +0200

    testing armadillo

--- beginner_files/test/test2/main.cpp
+++ beginner_files/test/test2/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <armadillo>
 
 using namespace std;
+
+using namespace arma;
 
 int main()
 {
     cout << "Hello World!" << endl;
+    vec x = randu<vec>(5);
+    cout << x << endl;
     return 0;
 }
 
