8bc3b2261ec956ed48bd2026d1927136d3fe4c1e
Ina Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2015-09-11 12:31:12 +0200

    testing converting int to string

--- beginner_files/test/test2/main.cpp
+++ beginner_files/test/test2/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <armadillo>
+#include <cstdlib>
+
 
 using namespace std;
 
@@ -7,9 +9,23 @@ using namespace arma;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    //cout << "Hello World!" << endl;
     vec x = randu<vec>(5);
-    cout << x << endl;
+    string a = "a";
+    string b = to_string(8);
+    string c =to_string(10)+a;
+
+
+    cout << c << endl;
+/*
+    ofstream myfile;
+        myfile.open ("arm_solve_10000.txt");
+        myfile << "x_mat" << "     " << "x2" << endl;
+        for (int i=0; i<5; i++){
+            myfile << x(i) << "    " << "test" << endl;
+        }
+        myfile.close();
+*/
     return 0;
 }
 
